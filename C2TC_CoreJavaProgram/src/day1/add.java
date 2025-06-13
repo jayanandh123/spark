cfcaef2850e74c9dd6b76816fca7962fcd97880c
jayanandh <jayanandh@DESKTOP-LAATRHC.mshome.net>
2025-06-13 10:32:50 +0530

    sub program

---
+++ C2TC_CoreJavaProgram/src/day1/add.java
@@ -0,0 +1,18 @@
+package day1;
+
+import java.util.Scanner;
+
+public class add {
+
+	public static void main(String[] args) {
+		Scanner gf = new Scanner(System.in);
+		System.out.print("enter a number:");
+		int a = gf.nextInt();
+		System.out.print("enter a number:");
+		int b =gf.nextInt();
+		int total = a-b;
+		System.out.println("Total is " +total);
+
+	}
+
+}
