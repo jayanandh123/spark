12a09f844421130ad50691dec77e052873c1bfb8
jayanandh <jayanandh@DESKTOP-LAATRHC>
2025-06-16 06:48:11 +0530

    the sample program for while

--- C2TC_CoreJavaProgram/src/day2/whiledemo.java
+++ C2TC_CoreJavaProgram/src/day2/whiledemo.java
@@ -1,10 +1,12 @@
 package day2;
-
 public class whiledemo {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		int i=99;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
 	}
-
 }
