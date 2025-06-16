146703f10c8bc20d972f0e6d88aba847ade261f0
jayanandh <jayanandh@DESKTOP-LAATRHC>
2025-06-16 06:41:25 +0530

    the sample program for forloop

--- C2TC_CoreJavaProgram/src/day2/forloopexample.java
+++ C2TC_CoreJavaProgram/src/day2/forloopexample.java
@@ -1,10 +1,12 @@
 package day2;
 
 public class forloopexample {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{	
+		for(int i = 1; i<=1000 ; i++)
+		{	
+			System.out.print("Value of i:");
+			System.out.println(i);
+		}
 	}
-
 }
